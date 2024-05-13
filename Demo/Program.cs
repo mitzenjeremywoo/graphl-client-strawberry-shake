a9e1bbf9f761ff64a43f4c9eebe4117e9d25ab84
jeremy woo <jeremy>
2024-05-13 20:55:17 +1200

    init samples.

---
+++ Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Demo.GraphQL;
+using StrawberryShake;
+
+namespace Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection
+                .AddConferenceClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+            IConferenceClient client = services.GetRequiredService<IConferenceClient>();
+        
+            var result = await client.GetSessions.ExecuteAsync();
+            result.EnsureNoErrors();
+
+            foreach (var session in result.Data.Sessions.Nodes)
+            {
+                Console.WriteLine(session.Title);
+                Console.WriteLine(session.Abstract);
+            }
+        }
+    }
+}
